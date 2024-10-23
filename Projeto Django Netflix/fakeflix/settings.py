c9f5438fe575fc3da624f1a6560430fad9d761aa
Edson Carvalho Inturia <30557202+edsoncarvalhointuria@users.noreply.github.com>
2024-10-23 12:47:08 -0300

    Update settings.py

--- Projeto Django Netflix/fakeflix/settings.py
+++ Projeto Django Netflix/fakeflix/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-zo)(_kv1+@3(z36-_=65)^n+gg6zc2^qo8n#4uh#j+z8!l2m@h
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -87,6 +88,14 @@ DATABASES = {
     }
 }
 
+import dj_database_url
+import os
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+if DATABASE_URL:
+    DATABASES = {
+        dj_database_url.config(default=DATABASE_URL, conn_max_age=1800)
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
@@ -124,6 +133,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/' #ISSO VEM POR PADRÃO
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
 STATICFILES_DIRS = [    #TEMOS QUE ADICIONAR ISSO PARA INDICAR O LOCAL DOS ARQUIVOS ESTATICOS
     BASE_DIR / "static"
 ]
