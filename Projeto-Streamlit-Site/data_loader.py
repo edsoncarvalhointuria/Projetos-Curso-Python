66f91eab988024dcf67d6f066d686715c41eb699
Edson Carvalho Inturia <30557202+edsoncarvalhointuria@users.noreply.github.com>
2025-03-19 17:40:15 -0300

    Atualizando caminho

--- Projeto-Streamlit-Site/data_loader.py
+++ Projeto-Streamlit-Site/data_loader.py
@@ -1,8 +1,9 @@
 import streamlit as st
 import pandas as pd
+from pathlib import Path
 
 
 @st.cache_data
 def carregar_base():
-    df = pd.read_excel("Base.xlsx")
+    df = pd.read_excel(Path(Path(__file__).absolute().parent,"Base.xlsx"))
     return df
