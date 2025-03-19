4e8544e8be63b03a2076fe0d8cce08e40a7f827c
Edson Carvalho Inturia <30557202+edsoncarvalhointuria@users.noreply.github.com>
2025-03-19 17:38:01 -0300

    Atualizando caminho

--- Projeto-Streamlit-Site/pages/homepage.py
+++ Projeto-Streamlit-Site/pages/homepage.py
@@ -1,9 +1,5 @@
 import streamlit as st
-
 from pathlib import Path
-st.write(f'Veja aqui amigo: {Path(__file__).absolute().parent.parent}')
-st.write(f'Veja aqui amigo: {Path(Path(__file__).absolute().parent.parent, "database/meubanco.db").exists()}')
-
 
 if st.session_state.get("name"):
     usuario = st.session_state.get("name").split()
@@ -20,4 +16,4 @@ if st.session_state.get("name"):
         st.switch_page("pages/indicadores.py")
 
     container = coluna_direita.container(border=True)
-    container.image("imagens/time-comunidade.jpeg", use_container_width=True)
+    container.image(Path(Path(__file__).absolute().parent.parent,("imagens/time-comunidade.jpeg")), use_container_width=True)
