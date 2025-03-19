43fcb22be4049d5f7bae182272401b38e9a88cd4
Edson Carvalho Inturia <30557202+edsoncarvalhointuria@users.noreply.github.com>
2025-03-19 17:08:24 -0300

    Atualizando caminho

--- Projeto-Streamlit-Site/pages/homepage.py
+++ Projeto-Streamlit-Site/pages/homepage.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 from pathlib import Path
-st.write(f'Veja aqui amigo: {Path(__file__).absolute()}')
+st.write(f'Veja aqui amigo: {Path(__file__).absolute().parent}')
 
 
 if st.session_state.get("name"):
